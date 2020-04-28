11e9aaba121a68b4d113c3cb8c1e3a175dbd41f6
JossWritesCode <joscelynrose@gmail.com>
2020-04-28 14:36:37 -0400

    initialized player on the board

--- util/sample_generator.py
+++ util/sample_generator.py
@@ -9,8 +9,15 @@ import math
 import random
 
 
+class Player:
+    def __init__(self, world):
+        self.x = world.width // 2
+        self.y = 0
+        self.sprite = " X "
+
+
 class Room:
-    def __init__(self, id, name, description, x_UL, y_UL, x_LR, y_LR):
+    def __init__(self, id, name, description, x_UL, y_UL, x_LR, y_LR, x_coordinate, y_coordinate):
         self.id = id
         self.name = name
         self.description = description
@@ -24,6 +31,8 @@ class Room:
         self.y_UL = y_UL
         self.x_LR = x_LR
         self.y_LR = y_LR
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
 
     def check_for_neighboring_rooms(self, world):
         # check to the north
@@ -47,42 +56,26 @@ class Room:
                 if world[self.x_UL - 1][y] is not None:
                     return "neighbor to the west"
 
-    # def __repr__(self):
-    #     if self.e_to is not None:
-    #         return f"({self.x}, {self.y}) -> ({self.e_to.x}, {self.e_to.y})"
-    #     return f"({self.x}, {self.y})"
-
-    # def connect_rooms(self, connecting_room, direction):
-    #     '''
-    #     Connect two rooms in the given n/s/e/w direction
-    #     '''
-    #     reverse_dirs = {"n": "s", "s": "n", "e": "w", "w": "e"}
-    #     reverse_dir = reverse_dirs[direction]
-    #     setattr(self, f"{direction}_to", connecting_room)
-    #     setattr(connecting_room, f"{reverse_dir}_to", self)
-
-    # def get_room_in_direction(self, direction):
-    #     '''
-    #     Connect two rooms in the given n/s/e/w direction
-    #     '''
-    #     return getattr(self, f"{direction}_to")
-
 
 class World:
-    def __init__(self):
+    def __init__(self, width=1, height=1):
         self.grid = None
-        self.width = 1
-        self.height = 1
+        self.width = width
+        self.height = height
+        self.x_UL = 0
+        self.y_UL = 0
+        self.x_LR = self.width
+        self.y_LR = -self.height
 
     def check_for_intersections(self, x_UL, y_UL, x_LR, y_LR):
         for y in range(y_UL, y_LR - 1, -1):
             for x in range(x_UL, x_LR):
                 #print( "check: (" + str(x) + "," + str(y) + ")")
                 if self.grid[y][x] is not None:
-                    print("intersection")
+                    #print( "intersection" )
                     return True
-        print("rect: %d, %d, %d, %d" % (x_UL, y_UL, x_LR, y_LR))
-        print("no intersections")
+        #print( "rect: %d, %d, %d, %d" % (x_UL, y_UL, x_LR, y_LR))
+        #print( "no intersections" )
         return False
 
     def generate_rooms(self, size_x, size_y, num_rooms):
@@ -91,21 +84,23 @@ class World:
         self.grid = [None] * size_y
         self.width = size_x
         self.height = size_y
+
         for i in range(len(self.grid)):
             self.grid[i] = [None] * size_x
 
-        room_count = 0
+        room_count = 1
         # initialize area of board left as the total area
         area_of_board_left = self.width * self.height
         while room_count < num_rooms:
 
-            print("room count: " + str(room_count))
+            #print( "room count: " + str( room_count) )
 
             number_left = num_rooms - room_count
-            max_length = math.sqrt(area_of_board_left - number_left)
+            max_length = int(math.sqrt(area_of_board_left - number_left) * 0.5)
 
-            #print( "width: " + str(width))
-            #print( "height: " + str(height))
+            # print("left: %d / %d" % (number_left, area_of_board_left))
+
+            # print( "max: %d" % (max_length,))
 
             # while loop
 
@@ -113,13 +108,18 @@ class World:
             height = 0
             while True:
 
-                width = max(1, int(random.randint(1, int(max_length)) * 0.25))
-                height = max(1, int(random.randint(1, int(max_length)) * 0.25))
+                width = max(1, int(random.randint(1, max_length) * 1) - 1)
+                #height = max( 1, int(random.randint( int(width * 0.5), min( int( width * 2 ), int(max_length) ) ) * 1) - 1 )
+                height = max(1, int(random.randint(1, max_length) * 1) - 1)
+
+                #print( "w: %d, h: %d" % (width, height))
 
                 room_point_x = random.randint(0, self.width - width)
                 #print("room_point_x: " + str(room_point_x))
-                room_point_y = random.randint(height, self.height - height)
+                room_point_y = random.randint(height - 1, self.height - 1)
                 #print("room_point_y: " + str(room_point_y))
+
+                #print( "x: %d, y: %d" % (room_point_x, room_point_y) )
 
                 if self.check_for_intersections(room_point_x, room_point_y, room_point_x + width, room_point_y - height) == False:
                     break
@@ -127,15 +127,15 @@ class World:
             # subtract room's area from running total
             area_of_board_left -= width * height
 
-            room = Room(room_count, "A Generic Room",
-                        "This is a generic room.", room_point_x, room_point_y, room_point_x + width, room_point_y - height)
             # Note that in Django, you'll need to save the room after you create it
 
             # Save the room in the World grid
-            print("w: %d, h: %d" % (width, height))
+            #print( "w: %d, h: %d" % (width, height) )
             for y in range(room_point_y, room_point_y - height, -1):
                 for x in range(room_point_x, room_point_x + width):
-                    print("place %d, %d" % (x, y))
+                    room = Room(room_count, "A Generic Room", "This is a generic room.",
+                                room_point_x, room_point_y, room_point_x + width, room_point_y - height, x, y)
+                    #print( "place %d, %d" % (x, y) )
                     self.grid[y][x] = room
 
             # self.grid[y][x] = room
@@ -148,42 +148,75 @@ class World:
             previous_room = room
             room_count += 1
 
-    def print_rooms(self):
+        for y in range(self.y_UL, self.y_LR - 1, -1):
+            for x in range(self.x_UL, self.x_LR):
+                empty_room = Room(0, "An Empty Space",
+                                  "This is an empty space", x, y, x, y, x, y)
+                #print( "check: (" + str(x) + "," + str(y) + ")")
+                if self.grid[y][x] is None:
+                    self.grid[y][x] = empty_room
+
+    def print_rooms(self, player):
         '''
         Print the rooms in room_grid in ascii characters.
         '''
 
         # Add top border
-        out = "# " * ((3 + self.width * 3) // 2) + "\n"
+        out = "# " * (((self.width * 3) // 2) + 1) + "#\n"
 
         # The console prints top to bottom but our array is arranged
         # bottom to top.
 
         reverse_grid = list(self.grid)  # make a copy of the list
         reverse_grid.reverse()
+
         for row in reverse_grid:
-            out += "# "
+            out += "#"
             for room in row:
+
                 if room is not None:
-                    out += " %02d" % (room.id,)
-                    #out += " " + str(room.id) + " "
+                    if room.id != 0:
+                        if room.x_coordinate == player.x and room.y_coordinate == player.y:
+                            out += player.sprite
+                        else:
+                            out += " %02d" % (room.id,)
+                    else:
+                        if room.x_UL == player.x and room.y_UL == player.y:
+                            out += player.sprite
+                        else:
+                            out += "   "
                 else:
                     out += "   "
             out += " #\n"
 
         # Add bottom border
-        out += "# " * ((3 + self.width * 3) // 2) + "\n"
+        out += "# " * (((self.width * 3) // 2) + 1) + "#\n"
 
         # Print string
         print(out)
 
+    def draw_sprite(self, player):
+        reverse_grid = list(self.grid)  # make a copy of the list
+        reverse_grid.reverse()
 
-w = World()
-num_rooms = 44
-width = 15
-height = 30
+        # for row in reverse_grid:
+        #     for room in row:
+        #         if player.x :
+        #             out += " %02d" % (room.id,)
+        #             #out += " " + str(room.id) + " "
+        #         else:
+        #             out += "   "
+        #     out += " #\n"
+
+
+num_rooms = 10
+width = 20
+height = 17
+w = World(width, height)
+j = Player(w)
+# print(j.x, j.y, "j")
 w.generate_rooms(width, height, num_rooms)
-w.print_rooms()
+w.print_rooms(j)
 
 
 print(
