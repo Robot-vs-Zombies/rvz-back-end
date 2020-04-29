7994601788a682ee30e4da58ecaab1f9161cc470
Andrew Ackerman <andrew.ackers@gmail.com>
2020-04-29 12:29:16 -0700

    set up backend
    Co-authored-by: emilybruner <bruner.emilym@gmail.com>
    Co-authored-by: Hail91 <aarongillies88@gmail.com>
    Co-authored-by: JossWritesCode <joscelynrose@gmail.com>

--- adventure/urls.py
+++ adventure/urls.py
@@ -6,7 +6,7 @@ from rest_framework.authtoken import views
 urlpatterns = [
     path('', include('rest_auth.urls')),
     path('registration/', include('rest_auth.registration.urls')),
-    # path('login/', views.obtain_auth_token),
+    path('login/', views.obtain_auth_token),
     url('init', api.initialize),
     url('move', api.move),
     url('say', api.say)
